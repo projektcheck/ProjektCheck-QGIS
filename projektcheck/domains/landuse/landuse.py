c7863dba1e29e2463cd21903a40171050db1f3b5
Christoph Franke <franke@ggr-planung.de>
2020-01-22 17:21:54 +0100

    landuse - parameters (save/load)

--- projektcheck/domains/landuse/landuse.py
+++ projektcheck/domains/landuse/landuse.py
@@ -1,4 +1,12 @@
 from projektcheck.base.domain import Domain
+from projektcheck.base.project import ProjectLayer
+from projektcheck.domains.definitions.tables import Teilflaechen
+from projektcheck.domains.landuse.tables import (WohnbaulandAnteile,
+                                                 WohnflaecheGebaeudetyp)
+from projektcheck.domains.constants import Nutzungsart
+from projektcheck.base.params import Params, Param, Title, Seperator
+from projektcheck.base.inputs import Slider
+from projektcheck.utils.utils import clearLayout
 
 
 class LandUse(Domain):
@@ -7,3 +15,102 @@ class LandUse(Domain):
     ui_label = 'Flächeninanspruchnahme'
     ui_file = 'ProjektCheck_dockwidget_analysis_05-Fl.ui'
     ui_icon = "images/iconset_mob/20190619_iconset_mob_domain_landuse_1.png"
+
+    def setupUi(self):
+        self.ui.area_combo.currentIndexChanged.connect(
+            lambda: self.change_area())
+        self.layout = self.ui.parameter_group.layout()
+
+    def load_content(self):
+        self.gebaeudetypen_base = self.basedata.get_table(
+            'Wohnen_Gebaeudetypen', 'Definition_Projekt'
+        )
+        self.areas = Teilflaechen.features().filter(
+            nutzungsart=Nutzungsart.WOHNEN.value)
+        self.wohnbauland_anteile = WohnbaulandAnteile.features(create=True)
+        self.wohnflaeche = WohnflaecheGebaeudetyp.features(create=True)
+
+        self.ui.area_combo.blockSignals(True)
+        self.ui.area_combo.clear()
+        for area in self.areas:
+            self.ui.area_combo.addItem(area.name, area)
+        self.ui.area_combo.blockSignals(False)
+
+        self.change_area()
+
+    def setup_params(self):
+        anteile = self.wohnbauland_anteile.get(id_teilflaeche=self.area.id)
+        value = anteile.anteil if anteile else 15
+        clearLayout(self.layout)
+        self.params = Params(self.layout,
+                             help_file='flaechennutzung_params.txt')
+        self.params.add(Title('Wohnbauland'))
+        self.params.wohnbauland_anteil = Param(
+            value, Slider(maximum=100),
+            label='Anteil der Fläche der ausgewählten Teilfläche,\n'
+            'welcher kein Nettowohnbauland\n'
+            '(= Wohnbaugrundstücke) ist',
+            unit='%'
+        )
+
+        self.params.add(Seperator())
+
+        self.params.add(Title('Durchschnittliche Wohnfläche je Wohneinheit'))
+
+        for bt in self.gebaeudetypen_base.features():
+            param_name = bt.param_we
+            feature = self.wohnflaeche.get(id_gebaeudetyp=bt.id,
+                                           id_teilflaeche=self.area.id)
+            value = feature.mean_wohnflaeche if feature else 0
+            self.params.add(Param(
+                value, Slider(maximum=200),
+                label=f'... in {bt.display_name}'),
+                name=param_name
+            )
+
+        self.params.changed.connect(self.save)
+        self.params.show()
+
+    def save(self):
+
+        feature = self.wohnbauland_anteile.get(id_teilflaeche=self.area.id)
+        # ToDo: get_or_create
+        if not feature:
+            feature = self.wohnbauland_anteile.add(id_teilflaeche=self.area.id)
+        feature.anteil = self.params.wohnbauland_anteil.value
+        feature.save()
+
+        for bt in self.gebaeudetypen_base.features():
+            feature = self.wohnflaeche.get(id_gebaeudetyp=bt.id,
+                                           id_teilflaeche=self.area.id)
+            if not feature:
+                feature = self.wohnflaeche.add(
+                    id_gebaeudetyp=bt.id, id_teilflaeche=self.area.id)
+            feature.mean_wohnflaeche = getattr(
+                self.params, bt.param_we).value
+            feature.save()
+
+    def change_area(self):
+
+        self.area = self.ui.area_combo.itemData(
+            self.ui.area_combo.currentIndex())
+        if not self.area:
+            return
+
+        output = ProjectLayer.find('Nutzungen des Plangebiets')
+        if output:
+            layer = output[0].layer()
+            layer.removeSelection()
+            layer.select(self.area.id)
+
+        self.setup_params()
+
+    def close(self):
+        # ToDo: implement this in project (collecting all used workscpaces)
+        output = ProjectLayer.find('Nutzungen des Plangebiets')
+        if output:
+            layer = output[0].layer()
+            layer.removeSelection()
+        if hasattr(self, 'areas'):
+            self.areas._table.workspace.close()
+        super().close()
---
+++ projektcheck/domains/landuse/tables.py
@@ -0,0 +1,22 @@
+from projektcheck.base.project import ProjectTable
+from projektcheck.base.database import Field
+
+
+class WohnbaulandAnteile(ProjectTable):
+
+    id_teilflaeche = Field(int, 0)
+    anteil = Field(float, 0)
+
+    class Meta:
+        workspace = 'definitions'
+
+
+class WohnflaecheGebaeudetyp(ProjectTable):
+
+    id_teilflaeche = Field(int, 0)
+    mean_wohnflaeche = Field(int, 0)
+    id_gebaeudetyp = Field(int, 0)
+    name_gebaeudetyp = Field(str, 0)
+
+    class Meta:
+        workspace = 'definitions'
