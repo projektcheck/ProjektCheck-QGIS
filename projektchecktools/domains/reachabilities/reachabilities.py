aa6c1c0f1444ea779e8f1e4dc14add33f2e4353e
Christoph Franke <franke@ggr-planung.de>
2020-02-28 09:03:56 +0100

    label erreichbarekeiot

--- projektchecktools/domains/reachabilities/reachabilities.py
+++ projektchecktools/domains/reachabilities/reachabilities.py
@@ -24,7 +24,7 @@ from projektchecktools.utils.utils import open_file
 class Reachabilities(Domain):
     """"""
 
-    ui_label = 'Erreichbarkeiten'
+    ui_label = 'Erreichbarkeit'
     ui_file = 'ProjektCheck_dockwidget_analysis_02-Err.ui'
     ui_icon = 'images/iconset_mob/20190619_iconset_mob_get_time_stop2central_2.png'
 
