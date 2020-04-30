cc944dbecf1448c872b6cf41d9606d2fe656be49
Christoph Franke <franke@ggr-planung.de>
2020-04-30 14:28:34 +0200

    fixed writing centers on project creation

--- projektchecktools/domains/marketcompetition/tables.py
+++ projektchecktools/domains/marketcompetition/tables.py
@@ -4,13 +4,13 @@ from projektchecktools.base.database import Field
 
 class Centers(ProjectTable):
 
-    name = Field(str, ''),
-    nutzerdefiniert = Field(int, -1),
-    umsatz_differenz = Field(int, 0),
-    umsatz_planfall = Field(int, 0),
-    umsatz_nullfall = Field(int, 0),
-    auswahl = Field(int, 0),
-    ags = Field(str, ''),
+    name = Field(str, '')
+    nutzerdefiniert = Field(int, -1)
+    umsatz_differenz = Field(int, 0)
+    umsatz_planfall = Field(int, 0)
+    umsatz_nullfall = Field(int, 0)
+    auswahl = Field(int, 0)
+    ags = Field(str, '')
     rs = Field(str, '')
 
     class Meta:
