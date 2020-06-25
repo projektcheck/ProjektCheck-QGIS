6fd491b796c834e641038a7f2753ce4adbeda74e
Christoph Franke <franke@ggr-planung.de>
2020-06-25 16:49:59 +0200

    refactored entry point

--- ProjektCheck.py
+++ ProjektCheck.py
@@ -1,110 +1,48 @@
 # -*- coding: utf-8 -*-
-from qgis.PyQt.QtCore import (QSettings, QTranslator, qVersion,
-                              QCoreApplication, Qt)
+from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
 from qgis import utils
 
-# Initialize Qt resources from file resources_rc.py
+# init resources
 from projektchecktools.ui.resources_rc import *
-
-# Import the code for the DockWidget
 from main_widget import ProjektCheckControl
-import os.path
 
 
 class ProjektCheck:
-    """QGIS Plugin Implementation."""
+    '''
+    land-use planning for urban and rural development
+    '''
 
     def __init__(self, iface=None):
-        # Save reference to the QGIS interface
         self.iface = iface or utils.iface
-        self.toolbar = self.iface.addToolBar(u'Projekt-Check')
-        self.toolbar.setObjectName(u'Projekt-Check')
+        self.toolbar = self.iface.addToolBar('Projekt-Check')
+        self.toolbar.setObjectName('Projekt-Check')
 
-        # initialize plugin directory
-        self.plugin_dir = os.path.dirname(__file__)
-
-        # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
-        locale_path = os.path.join(
-            self.plugin_dir,
-            'i18n',
-            'ProjektCheck_{}.qm'.format(locale))
-
-        if os.path.exists(locale_path):
-            self.translator = QTranslator()
-            self.translator.load(locale_path)
-
-            if qVersion() > '4.3.3':
-                QCoreApplication.installTranslator(self.translator)
-
-        # Declare instance attributes
         self.actions = []
-        self.menu = self.tr(u'&Projekt-Check')
-
-        #print "** INITIALIZING ProjektCheck"
+        self.menu = 'Projekt-Check'
 
         self.pluginIsActive = False
         self.mainwidget = None
         self.drawwidget = None
         self.toolbuttonwidget = None
 
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('Projekt-Check', message)
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
-
     def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
+        '''
+        override, add entry points (actions) for the plugin
+        '''
 
         icon_path = ':/images/images/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'Projekt-Check'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
-
-    #--------------------------------------------------------------------------
+        icon = QIcon(icon_path)
+        action = QAction(icon, 'Projekt-Check', self.iface.mainWindow())
+        action.triggered.connect(lambda: self.run())
+        self.toolbar.addAction(action)
+        self.iface.addPluginToMenu('Geokodierung', action)
 
     def onClosePlugin(self):
-        """Cleanup necessary items here when plugin dockwidget is closed"""
+        '''
+        override, close UI on closing plugin
+        '''
 
         # disconnects
         self.mainwidget.closing_widget.disconnect(self.onClosePlugin)
@@ -113,14 +51,12 @@ class ProjektCheck:
         self.pluginIsActive = False
 
     def unload(self):
-        """Removes the plugin menu item and icon from QGIS GUI."""
-
-        print("** UNLOAD ProjektCheck")
+        '''
+        remove the plugin and its UI from the QGIS interface
+        '''
 
         for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&Projekt-Check'),
-                action)
+            self.iface.removePluginMenu('Projekt-Check', action)
             self.iface.removeToolBarIcon(action)
         # remove the toolbar
         if self.toolbar:
@@ -133,10 +69,10 @@ class ProjektCheck:
             self.mainwidget = None
         self.pluginIsActive = False
 
-    #--------------------------------------------------------------------------
-
     def run(self):
-        """Run method that loads and starts the plugin"""
+        '''
+        open the plugin UI
+        '''
         if self.pluginIsActive:
             return
 
--- main_widget.py
+++ main_widget.py
@@ -23,7 +23,7 @@ from projektchecktools.domains.definitions.tables import Projektrahmendaten
 
 class ProjektCheckControl(PCDockWidget):
 
-    ui_file = '_base.ui'
+    ui_file = 'base.ui'
 
     def setupUi(self):
         #self.ui.pandas_button.clicked.connect(self.install_pandas)
